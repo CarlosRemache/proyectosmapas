b2d402b5bcc9d0286a4ba362abdffe3b234871c7
CarlosRemache <carlosrsua@gmail.com>
2025-11-19 16:18:01 -0500

    nuevos comando

--- proyectos/settings.py
+++ proyectos/settings.py
@@ -28,11 +28,23 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'proyectosmapas.onrender.com',
+    os.environ.get('RENDER_EXTERNAL_HOSTNAME', ''),
+    'localhost',
+    '127.0.0.1'
+]
+
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 
 # Application definition
 
