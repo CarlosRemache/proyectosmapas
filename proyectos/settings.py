253323aee6b093a7f6c1e6f10d809ff45ddd52c5
CarlosRemache <carlosrsua@gmail.com>
2025-12-30 03:14:05 -0500

    nuevos

--- proyectos/settings.py
+++ proyectos/settings.py
@@ -61,23 +61,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'channels', #activar el chanel django
-    'cloudinary',
-    'cloudinary_storage',
-    'Aplicaciones.proyectos',
+    'Aplicaciones.proyectos'
 ]
-
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
-}
-
-
-
-
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -214,8 +199,8 @@ if not DEBUG:
 
 
 #CONFIGURANDO CARPETA PARA SUBIR ARCHIVOS
-#MEDIA_URL='/media/'
-#MEDIA_ROOT=(os.path.join(BASE_DIR,'proyectos/media/'))
+MEDIA_URL='/media/'
+MEDIA_ROOT=(os.path.join(BASE_DIR,'proyectos/media/'))
 
 
 
